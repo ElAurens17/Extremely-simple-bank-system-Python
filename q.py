949c31a70593ca04392461bd394f69a3ba4fd79c
Lawrence Dacut <lawrencedacut20@gmail.com>
2023-07-09 14:36:36 +0800

    q

---
+++ q.py
@@ -0,0 +1,8 @@
+def inv():
+    x = int(input("How much would you like to invest? "))
+    y = int(input("How much would you like to withdraw? "))
+    z = x - y
+    print("Your balance is: " + str(z))
+
+inv()
+
