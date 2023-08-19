435de037583c463bb815376ab5ccd2fa862f073e
Lawrence Dacut <lawrencedacut20@gmail.com>
2023-08-19 15:00:46 +0800

    2

--- q.py
+++ q.py
@@ -1,8 +1,22 @@
-def inv():
-    x = int(input("How much would you like to invest? "))
+def deposit(balance):
+    x = int(input("How much would you like to deposit? "))
+    print("Deposit is " + str(x))
+    new_balance = balance + x
+    print("Your balance is: " + str(new_balance))
+    return new_balance
+
+def withdraw(balance):
     y = int(input("How much would you like to withdraw? "))
-    z = x - y
-    print("Your balance is: " + str(z))
+    if y > balance:
+        print("Insufficient balance.")
+    else:
+        r = balance - y
+        print("Total balance after withdrawal: " + str(r))
+        return r
 
-inv()
+def bank():
+    balance = 0
+    balance = deposit(balance)
+    balance = withdraw(balance)
 
+bank()
